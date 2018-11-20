ab922374961d6ce4a27cba1d6a1c6a06920c76ad
Yuta Matsumura <y-matsumura@alterbooth.com>
2018-11-20 19:44:51 +0900

    setup session store

--- MokumokuIdentityApp/Startup.cs
+++ MokumokuIdentityApp/Startup.cs
@@ -42,6 +42,18 @@ namespace MokumokuIdentityApp
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
+            // auth session store
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                options.LoginPath = "/Identity/Account/Login";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 

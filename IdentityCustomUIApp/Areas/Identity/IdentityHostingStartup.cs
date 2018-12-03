fd19acdf78bff9ca731d556ca5ef791126138bc8
Yuta Matsumura <y-matsumura@alterbooth.com>
2018-12-03 20:56:57 +0900

    Custom Identity UI

---
+++ IdentityCustomUIApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using IdentityCustomUIApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdentityCustomUIApp.Areas.Identity.IdentityHostingStartup))]
+namespace IdentityCustomUIApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

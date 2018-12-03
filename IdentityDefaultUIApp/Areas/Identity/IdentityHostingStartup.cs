591c67515ef59b7bdb1ce7ddd079155b6ff8b619
Yuta Matsumura <y-matsumura@alterbooth.com>
2018-12-03 20:19:29 +0900

    Default UI Identity App

---
+++ IdentityDefaultUIApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using IdentityDefaultUIApp.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdentityDefaultUIApp.Areas.Identity.IdentityHostingStartup))]
+namespace IdentityDefaultUIApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityDefaultUIAppIdentityDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityDefaultUIAppIdentityDbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<IdentityDefaultUIAppIdentityDbContext>();
+            });
+        }
+    }
+}
